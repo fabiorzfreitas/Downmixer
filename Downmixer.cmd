3af45f07eee6d13eb6f3d5109dba46376bfb3e83
fabiorzfreitas <mckfabio@gmail.com>
2022-02-17 15:37:40 -0300

    Working, but with findstr
    findstr has some limitations that must be worked around

--- Downmixer.cmd
+++ Downmixer.cmd
@@ -1,14 +1,29 @@
 @echo off
-for /f in () do (
-    ffmpeg -i "%%~fg" -filter_complex "[0:a:0]volume=1.66,pan=stereo|FL=0.5*FC+0.707*FL+0.707*BL+0.5*LFE|FR=0.5*FC+0.707*FR+0.707*BR+0.5*LFE[filtered]" -map 0:v -c:v copy -map [filtered] -c:a:0 ac3 -metadata:s:a:0 title="Stereo 2.0" -metadata:s:a:0 language=eng -map 0:a:0 -c:a:1 copy -disposition:a:1 0 "%%~dpnG.downmix%%~xG"
-    if not errorlevel 1 (
-    	echo --^> Deleting old file
-        del /f "%%~fG"
-    	echo --^> Renaming new file
-    	ren "%%~dpnG.downmix%%~xG" "%%~nxG"
-    ) else (
-        echo Warnings/errors generated during remuxing, original file not deleted, check errors.txt
-        mkvmerge.exe -i --ui-language en "%%~fG" >> Errors.txt
-        del "%%~dpnG.downmix%%~xG" 2>nul
+chcp 65001
+if not exist "%~dpn0.log" break >"%~dpn0.log"
+set "FileList=%~dp0FileList.txt"
+for /f "usebackq delims=" %%G in ("%FileList%") do (
+    %systemroot%\system32\findstr.exe /xlc:"%%G" "%~dpn0.log" >nul && (
+        echo "%%~nG" has already been processed
+        echo.
+        echo ##########
+        echo.
+    ) || (
+        echo --^> Processing file "%%~nG"
+        ffmpeg -i "%%~fG" -filter_complex "[0:a:0]volume=1.66,pan=stereo|FL=0.5*FC+0.707*FL+0.707*BL+0.5*LFE|FR=0.5*FC+0.707*FR+0.707*BR+0.5*LFE[filtered]" -map 0:v -c:v copy -map [filtered] -c:a:0 ac3 -metadata:s:a:0 title="Stereo 2.0" -metadata:s:a:0 language=eng -map 0:a:0 -c:a:1 copy -disposition:a:1 0 "%%~dpnG.downmix%%~xG"
+        if not errorlevel 1 (
+    	    echo --^> Deleting old file
+            del /f "%%~fG"
+    	    echo --^> Renaming new file
+    	    ren "%%~dpnG.downmix%%~xG" "%%~nxG"
+        ) else (
+            echo Warnings/errors generated during remuxing, original file not deleted, check errors.txt
+            mkvmerge.exe -i --ui-language en "%%~fG" >> Errors.txt
+            del "%%~dpnG.downmix%%~xG" 2>nul
+        )
+        echo.
+        echo ##########
+        echo.
+        >>"%~dpn0.log" echo %%G
     )
-)
+ )
---
+++ GetFiles.cmd
@@ -0,0 +1,2 @@
+chcp 65001
+dir *.mkv /s /b>FileList.txt
